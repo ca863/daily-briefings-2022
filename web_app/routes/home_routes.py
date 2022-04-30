c00476028e9158304c73df25c389e48a98110743
ca863 <ca863@georgetown.edu>
2022-04-30 15:05:51 -0400

    Hello Update new route

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -21,3 +21,19 @@ def about():
 def another():
     print("ANOTHER PAGE MAYBE...")
     return "Here is another page"
+
+    # web_app/routes/home_routes.py
+
+# ...
+
+@home_routes.route("/hello")
+def hello_world():
+    print("HELLO...")
+    url_params = dict(request.args)
+    print("URL PARAMS:", url_params)
+    
+    name = url_params.get("name") or "World"
+    
+    message = f"Hello, {name}!"
+    return message
+    #return render_template("hello.html", message=message)
